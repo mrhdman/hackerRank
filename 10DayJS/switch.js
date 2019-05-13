1f536c7997a2c10d7d857ee959890e5b055f06d4
mrhdman <mattrhavins@gmail.com>
2019-05-12 21:40:03 -0400

    switch

--- 10DayJS/switch.js
+++ 10DayJS/switch.js
@@ -1,42 +1,35 @@
 function getLetter(s) {
     let letter;
-    // var vowels = ["a", "e", "i", "o", "u"];
-    // var cons1 = ["b", "c", "d", "e", "f", "g"];
-    // var cons2 = ["h", "j", "k", "l", "m"];
     let firstLetter = s.charAt(0);
-    let Atest = false;
-    let Btest = false;
-    let Ctest = false;
-    let Dtest = false;
 
-    if (firstLetter = "a" || "e" || "i" || "o" || "u") {
-        Atest = true;
-    } else if (firstLetter = "b" || "c" || "d" || "f" || "g") {
-        Btest = true;
-    } else if (firstLetter = "h" || "j" || "k" || "l" || "m") {
-        Ctest = true;
-    } else {
-        Dtest = true;
-    }
     switch(firstLetter) {
-        case Atest = true:
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
             letter = "A";
-            console.log(letter);
             break;
-        case Btest = true:
+        case "b":
+        case "c":
+        case "d":
+        case "e":
+        case "f":
+        case "g":
             letter = "B";
-            console.log(letter);
             break;
-        case Ctest = true:
+        case "h":
+        case "j":
+        case "k":
+        case "l":
+        case "m":
             letter = "C";
-            console.log(letter);
             break;
         default:
             letter = "D";
-            console.log(letter);
             break;
     }
-    return letter;
+    console.log(letter);
 }
 
 getLetter("abcedkjfs");
