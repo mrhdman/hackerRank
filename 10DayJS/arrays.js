eb360cbb5407aedb1195057eb4c9c5af3efe757b
mrhdman <mattrhavins@gmail.com>
2019-05-13 15:30:08 -0400

    arrays

---
+++ 10DayJS/arrays.js
@@ -0,0 +1,19 @@
+function getSecondLargest(nums) {
+    var largest = 0;
+    var second = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > largest){
+            largest = nums[i];
+        }
+    }
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > second && nums[i] < largest){
+            second = nums[i];
+        }
+    }
+    return second;
+}
+
+getSecondLargest(82928);
+
